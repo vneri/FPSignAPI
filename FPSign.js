579d86646c19a99ca648db751b987ca5b95e0c7b
Valerio <valerioneri.de@gmail.com>
2020-01-07 13:49:47 +0100

    created the file

---
+++ FPSign.js
@@ -0,0 +1,58 @@
+// provides UI droplets for FP Sign and API calls
+
+
+var FPSign={};
+FPSign.baseURL = 'https://app.fp-sign.com/api/v1_2';
+FPSign.login = function(username, password){
+	return new Promise(function(resolve, reject){
+		var payload = 
+			{
+				"username": username,
+				"password": password
+			}
+		Xhr.POST(FPSign.baseURL + '/login', function(data){
+			resolve(data);
+		},payload, 'application/json');
+	});
+}
+
+FPSign.register = function(username, password, firstName, lastName, language, country, resellerId, campaignName, leadSource){
+	return new Promise(function(resolve, reject){
+		var payload = 
+			{
+				"username": username,
+				"password": password,
+				"first_name": firstName,
+				"last_name": lastName,
+				"language":language,
+				"country": country,
+				"reseller_id": resellerId,
+				"campaign_name": campaignName,
+				"lead_source": leadSource
+			}
+		Xhr.POST(FPSign.baseURL + '/registration', function(data){
+			resolve(data);
+		},payload, 'application/json');
+	});
+}
+
+FPSign.createForm =  function(formType, id, cssClasses){
+	var formHTML = '';
+	if(! (cssClasses && cssClasses['text']) ){
+		var cssClasses = [];
+		cssClasses['text'] = 'FPSignFormTextInput';
+	}
+	switch(formType){
+		case 'register':
+			formHTML =
+			'<form><input type="text" class="'+cssClasses['text']+'" /><input type="text" class="'+cssClasses['text']+'" /></form>';
+		break;
+		case 'login':
+			formHTML =
+			'<form><input type="text" class="'+cssClasses['text']+'" /><input type="text" class="'+cssClasses['text']+'" /></form>';
+		break;
+	}
+	if (formHTML != "")
+		document.getElementById(id).innerHTML = formHTML;
+		
+}
