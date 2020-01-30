f8fb2ed3a55dd5906c49644f2acd5b131032162a
Valerio <valerioneri.de@gmail.com>
2020-01-30 13:09:00 +0100

    Updated and integrated the AJAX wrapper

--- FPSign.js
+++ FPSign.js
@@ -1,18 +1,18 @@
-// provides UI droplets for FP Sign and API calls
-
-
 var FPSign={};
 FPSign.baseURL = 'https://app.fp-sign.com/api/v1_2';
 FPSign.login = function(username, password){
 	return new Promise(function(resolve, reject){
 		var payload = 
 			{
-				"username": username,
+				"username": decodeURIComponent(username),
 				"password": password
 			}
-		Xhr.POST(FPSign.baseURL + '/login', function(data){
-			resolve(data);
-		},payload, undefined, 'application/json');
+        FPSign.Xhr.POST(FPSign.baseURL + '/login', function(data){
+          if (data.status < 300)
+          	resolve(data.body);
+          else
+            reject(data.body);
+        },payload, undefined, 'application/json');
 	});
 }
 
@@ -20,39 +20,77 @@ FPSign.register = function(email, password, firstName, lastName, language, count
 	return new Promise(function(resolve, reject){
 		var payload = 
 			{
-				"email": email,
+				"email": decodeURIComponent(email),
 				"password": password,
 				"first_name": firstName,
 				"last_name": lastName,
 				"language":language,
 				"country": country,
 				"reseller_id": resellerId,
-				"campaign_name": campaignName,
-				"lead_source": leadSource
 			}
-		Xhr.POST(FPSign.baseURL + '/registration', function(data){
-			resolve(data);
-		},payload, undefined, 'application/json');
+        if (campaignName != undefined){
+          payload["campaign_name"] = campaignName;
+        }
+      	if (leadSource != undefined){
+          payload["lead_source"] = leadSource;
+        }
+      	
+        FPSign.Xhr.POST(FPSign.baseURL + '/registration', function(data){
+          if (data.status < 300)
+          	resolve(data.body);
+          else
+            reject(data.body);
+        },payload, undefined, 'application/json');
 	});
 }
 
-FPSign.createForm =  function(formType, id, cssClasses){
-	var formHTML = '';
-	if(! (cssClasses && cssClasses['text']) ){
-		var cssClasses = [];
-		cssClasses['text'] = 'FPSignFormTextInput';
-	}
-	switch(formType){
-		case 'register':
-			formHTML =
-			'<form><input type="text" class="'+cssClasses['text']+'" /><input type="text" class="'+cssClasses['text']+'" /></form>';
-		break;
-		case 'login':
-			formHTML =
-			'<form><input type="text" class="'+cssClasses['text']+'" /><input type="text" class="'+cssClasses['text']+'" /></form>';
-		break;
-	}
-	if (formHTML != "")
-		document.getElementById(id).innerHTML = formHTML;
+FPSign.Xhr={	
+	GET:function(url, callback, body, auth){
+		var xmlhttp;
+		if (window.XMLHttpRequest){// code for IE7+, Firefox, Chrome, Opera, Safari
+			xmlhttp=new XMLHttpRequest();
+		} else {// code for IE6, IE5
+			xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+		}		
 		
+		
+		xmlhttp.onreadystatechange=function(){
+          if (xmlhttp.readyState==4){
+            var response = {};
+            response.body = xmlhttp.responseText;
+            response.status = xmlhttp.status;
+            callback(response);
+          }
+		}
+		
+		xmlhttp.open("GET",url,true);
+		if (auth != undefined)
+			xmlhttp.setRequestHeader("Authorization", "Basic " + btoa(auth['username'] + ":" + auth['password']));
+		xmlhttp.send(body);
+	},
+
+	POST:function(url, callback, body, auth, contentType){
+		var xmlhttp;
+		if (window.XMLHttpRequest){// code for IE7+, Firefox, Chrome, Opera, Safari
+			xmlhttp=new XMLHttpRequest();
+		} else {// code for IE6, IE5
+			xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+		}		
+
+		xmlhttp.onreadystatechange=function(){
+          if (xmlhttp.readyState==4){
+            var response = {};
+            response.body = xmlhttp.responseText;
+            response.status = xmlhttp.status;
+            callback(response);
+          }
+		}
+		
+		xmlhttp.open("POST",url,true);
+		if (contentType != undefined)
+			xmlhttp.setRequestHeader("Content-Type", contentType);
+		if (auth != undefined)
+			xmlhttp.setRequestHeader("Authorization", "Basic " + btoa(auth['username'] + ":" + auth['password']));
+		xmlhttp.send(JSON.stringify(body));
+	}
 }
